dbd2503591ad8545c9cefa7437a690c164628dae
MdAfsarHossain <afsar291722@gmail.com>
2024-10-12 10:27:37 +0600

    Statistics jsx file added

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Statistics = () => {
+  return (
+    <div className="px-10 py-28 md:py-24 lg:py-28">
+      <div className="flex flex-col items-center justify-center py-10 border-b-2 mb-5">
+        <h1 className="text-5xl font-bold">Statistics</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
