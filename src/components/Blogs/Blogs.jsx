4e2cf4f899818991533bbe9a44c5d8e563cb47ab
MdAfsarHossain <afsar291722@gmail.com>
2024-10-12 10:31:25 +0600

    Blogs jsx file added

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div className="px-10 py-28 md:py-24 lg:py-28">
+      <div className="flex flex-col items-center justify-center py-10 border-b-2 mb-5">
+        <h1 className="text-5xl font-bold">Blogs</h1>
+      </div>
+
+
+      <div className="flex flex-row justify-center items-center text-3xl md:text-5xl font-bold text-green-500">
+        <h1>Comming Soon......</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
